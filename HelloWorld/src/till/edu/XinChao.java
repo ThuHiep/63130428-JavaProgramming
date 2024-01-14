f9145efde8a7594fbe2215dcbb985169ea8ce9f9
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-01-14 20:47:40 +0700

    ví dụ đầu tiên
    Chỉ để Demo

---
+++ HelloWorld/src/till/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package till.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Tôi tên là Thu Hiệp");
+
+	}
+
+}
