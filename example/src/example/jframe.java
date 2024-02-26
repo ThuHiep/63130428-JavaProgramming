dbef7aee280b81001a7897f600b7bd4cee28cbe7
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-02-26 15:09:02 +0700

    bài Thực Hành

---
+++ example/src/example/jframe.java
@@ -0,0 +1,51 @@
+package example;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
+
+public class jframe extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					jframe frame = new jframe();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public jframe() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JEditorPane dtrpnNhpA = new JEditorPane();
+		dtrpnNhpA.setText("Nhập a:");
+		contentPane.add(dtrpnNhpA);
+		
+		JButton btnNewButton = new JButton("Tổng");
+		contentPane.add(btnNewButton);
+	}
+
+}
